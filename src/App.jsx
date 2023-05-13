ca5bd9e891efb02cefaba28301ff03e5343ddfd7
Suleman-Tasawar <sulemantasawar9@gmail.com>
2023-05-13 12:32:25 +0500

    Set up thev proj dir

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-600 font-bold">
+        Firing up the Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
