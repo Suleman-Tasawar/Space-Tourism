647854f1b367dc9febd17b953966dbf25908e73e
Suleman-Tasawar <sulemantasawar9@gmail.com>
2023-05-19 11:36:49 +0500

    Feature--Added the multi tabs feature

--- src/components/Destinations.jsx
+++ src/components/Destinations.jsx
@@ -1,17 +1,21 @@
 // eslint-disable-next-line react/prop-types
 function Destination({ img, title, text, distance, time }) {
   return (
-    <div className="flex justify-evenly items-end">
+    <div className="flex justify-evenly items-center mt-28">
       <div>
-        <img src={img} alt="Image of Planet to Visit" />
+        <img
+          className="w-[445px] h-[445px]"
+          src={img}
+          alt="Image of Planet to Visit"
+        />
       </div>
 
-      <div>
-        <h1 className="text-4xl text-white">{title}</h1>
-        <p className="text-NearDarkGrey">{text}</p>
-        <div className="flex justify-between items-center">
+      <div className="w-[445px] flex flex-col justify-center items-middle">
+        <h1 className="text-8xl text-white">{title}</h1>
+        <p className="text-NearDarkGrey mt-12">{text}</p>
+        <div className="flex justify-between items-center border-t-2 border-gray-600 mt-12">
           <div>
-            <p className="text-NearDarkGrey">Average Distance</p>
+            <p className="text-NearDarkGrey text-left">Average Distance</p>
             <h1 className="text-2xl text-white">{distance}</h1>
           </div>
 
--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import Destination from "../components/Destinations";
+import { useState } from "react";
 
 function Destinations() {
-  const [tabs, setTabs] = useState(0);
+  const [showsDestination, setShowDestination] = useState(null);
 
   const styles = {
     height: "100vh",
@@ -12,43 +12,45 @@ function Destinations() {
     backgroundSize: "cover",
   };
 
-  //Data of Planets
-  const destinationData = {
-    moon: {
-      id: 1,
+  //Data of destinations
+  const data = [
+    {
+      id: 0,
       image: "/assets/destination/image-moon.png",
       title: "MOON",
       text: "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
       distance: "384,400 km",
       time: "3 DAYS",
     },
-
-    mars: {
-      id: 2,
+    {
+      id: 1,
       image: "/assets/destination/image-mars.png",
       title: "MARS",
       text: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
       distance: "225 MIL. km",
       time: "9 MONTHS",
     },
-
-    europa: {
-      id: 3,
+    {
+      id: 2,
       image: "/assets/destination/image-europa.png",
       title: "EUROPA",
       text: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
       distance: "628 MIL. km",
       time: "3 YEARS",
     },
-
-    titan: {
-      id: 4,
+    {
+      id: 3,
       image: "/assets/destination/image-titan.png",
       title: "TITAN",
       text: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
       distance: "1.6 BIL. km",
       time: "7 YEARS",
     },
+  ];
+
+  const showDestination = (key) => {
+    const selectedDestination = data.find((element) => element.id === key);
+    setShowDestination(selectedDestination);
   };
 
   return (
@@ -59,25 +61,44 @@ function Destinations() {
 
       <nav className="relative">
         <ul className="flex absolute top-32 right-32">
-          <li className="text-NearDarkGrey ml-6 hover:text-white hover:underline cursor-pointer ">
+          <li
+            className="text-NearDarkGrey ml-6 hover:text-white hover:underline cursor-pointer "
+            onClick={() => showDestination(0)}
+          >
             MOON
           </li>
-          <li className="text-NearDarkGrey ml-6 hover:text-white hover:underline cursor-pointer">
+          <li
+            className="text-NearDarkGrey ml-6 hover:text-white hover:underline cursor-pointer"
+            onClick={() => showDestination(1)}
+          >
             MARS
           </li>
-          <li className="text-NearDarkGrey ml-6 hover:text-white hover:underline cursor-pointer">
+          <li
+            className="text-NearDarkGrey ml-6 hover:text-white hover:underline cursor-pointer"
+            onClick={() => showDestination(2)}
+          >
             EUR0PA
           </li>
-          <li className="text-NearDarkGrey ml-6 hover:text-white hover:underline cursor-pointer">
+          <li
+            className="text-NearDarkGrey ml-6 hover:text-white hover:underline cursor-pointer"
+            onClick={() => showDestination(3)}
+          >
             TITAN
           </li>
         </ul>
       </nav>
-      <div>{
-        Object.keys(planetData).map((data) =>(
-          
-        ))
-        }</div>
+
+      <div>
+        {showsDestination && (
+          <Destination
+            img={showsDestination.image}
+            title={showsDestination.title}
+            text={showsDestination.text}
+            distance={showsDestination.distance}
+            time={showsDestination.time}
+          />
+        )}
+      </div>
     </div>
   );
 }
